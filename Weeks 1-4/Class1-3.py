c68348f3e88336b62bcb6e45743b8edb9bc155d3
Adam-mlkamp <117931321+Adam-mlkamp@users.noreply.github.com>
2022-12-04 20:18:06 -0700

    Delete Weeks 1-4 directory

--- Weeks 1-4/Class1-3.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def compute_area(r):
-    return math.pi * r * r
-
-radius = float(input('Radius: '))
-print(f"The area is: {compute_area(radius)}")
--- Weeks 1-4/Teamactivity.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-current_time_and_date = datetime.now()
-day_of_week = current_time_and_date.weekday()
-print(day_of_week)
